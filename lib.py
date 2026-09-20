6bc973de0c31a6f56d2e566d0b5a9441f8c7f34e
Valik <kvaliak13@gmail.com>
2026-09-20 17:17:44 +0300

    Add multiplication function

--- lib.py
+++ lib.py
@@ -1,6 +1,9 @@
 def add_numbers(a, b):
     """Додає два числа та повертає результат."""
     return a + b
+def multiply_numbers(a, b):
+    """Множить два числа та перевіряє результат."""
+    return a*b
 def greet(name):
     """Створює привітання для користувача."""
     return f"Привіт, {name}!"
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from lib import add_numbers,greet 
+from lib import add_numbers,multiply_numbers,greet
 def main ():
     """Головна функція програми."""
     result = add_numbers(5, 7)
     print (f"Результат додавання:{result}")
+    multiplication=multiply_numbers(3,4)
+    print (f"Результат множення:{multiplication}")
 
     massage = greet ("Студент")
     print (massage)
