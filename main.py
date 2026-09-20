78eb5df487352d9a379d24b07e64a1761d534a51
Valik <kvaliak13@gmail.com>
2026-09-20 18:22:27 +0300

    Add square number function

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from lib import add_numbers,multiply_numbers,greet
+from lib import add_numbers,multiply_numbers,greet,square_number
 def main ():
     """Головна функція програми."""
     result = add_numbers(5, 7)
     print (f"Результат додавання:{result}")
     multiplication=multiply_numbers(3,4)
     print (f"Результат множення:{multiplication}")
+    square =square_number(5)
+    print(f"Квадрат числа:{square}")
 
     massage = greet ("Студент")
     print (massage)
